d84d0487822c46ddb323036bfb469155015369d4
mattzamat <96935285+mattzamat@users.noreply.github.com>
2022-04-23 17:29:18 -0400

    Update costperwatt.py

--- costperwatt.py
+++ costperwatt.py
@@ -36,7 +36,7 @@ with st.container():
         st.header('Enter Project Costs')
 
 with st.container():
-    col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
     if next_step:
         with col1:
             module = st.number_input(label="Module", step=0.1)
